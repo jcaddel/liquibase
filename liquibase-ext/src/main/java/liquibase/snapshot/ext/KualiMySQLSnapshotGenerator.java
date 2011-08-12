4e6ec8be303140f5976ef185324678f857b561e3
Jeff Caddel <jcaddel@yahoo.com>
2011-08-12 13:19:27 -0700

    Start of Liquibase extensions library

---
+++ liquibase-ext/src/main/java/liquibase/snapshot/ext/KualiMySQLSnapshotGenerator.java
@@ -0,0 +1,60 @@
+package liquibase.snapshot.ext;
+
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+import liquibase.database.Database;
+import liquibase.database.structure.View;
+import liquibase.exception.DatabaseException;
+import liquibase.snapshot.DatabaseSnapshot;
+import liquibase.snapshot.jvm.MySQLDatabaseSnapshotGenerator;
+
+/**
+ * Provides MySQL specific logic for capturing a snapshot of a database via JDBC
+ *
+ * @author Jeff Caddel
+ */
+public class KualiMySQLSnapshotGenerator extends MySQLDatabaseSnapshotGenerator {
+    public static final int KUALI_DB_PRIORITY = 6;
+
+    public int getPriority() {
+        return KUALI_DB_PRIORITY;
+    }
+
+    /**
+     * When a view is created in MySQL, the original SQL statement is parsed, and has some formatting applied to it
+     * before being stored. The parsed version always contains the database name prefixed to the table names eg "select
+     * * from `foo`.`bar`" This makes the SQL non-portable if you want to extract it and use it in another database. The
+     * logic here, strips the database name from the SQL. It changes "select * from `foo`.`bar`" to "select * from bar"
+     */
+    @Override
+    protected void readViews(DatabaseSnapshot snapshot, String schema, DatabaseMetaData databaseMetaData)
+            throws SQLException, DatabaseException {
+
+        // Fill in views using the logic from the super class
+        super.readViews(snapshot, schema, databaseMetaData);
+
+        Database db = snapshot.getDatabase();
+        String convertedSchema = db.convertRequestedSchemaToSchema(schema);
+        String quotedSchema = "`" + convertedSchema.toLowerCase() + "`.";
+        String[] tokens = { quotedSchema, "`" };
+
+        // Iterate through the view definitions and strip out the database name
+        for (View view : snapshot.getViews()) {
+            String definition = view.getDefinition().trim();
+            String newDefinition = replaceTokens(definition, tokens, "");
+            view.setDefinition(newDefinition);
+        }
+    }
+
+    protected String replaceTokens(String s, String[] tokens, String replacement) {
+        if (tokens == null) {
+            return s;
+        }
+        for (String token : tokens) {
+            s = s.replace(token, replacement);
+        }
+        return s;
+    }
+
+}
---
+++ liquibase-ext/src/main/java/liquibase/sqlgenerator/ext/InsertGeneratorOracle.java
@@ -0,0 +1,204 @@
+package liquibase.sqlgenerator.ext;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import liquibase.database.Database;
+import liquibase.database.core.OracleDatabase;
+import liquibase.database.typeconversion.TypeConverter;
+import liquibase.database.typeconversion.TypeConverterFactory;
+import liquibase.sql.Sql;
+import liquibase.sql.UnparsedSql;
+import liquibase.sqlgenerator.SqlGeneratorChain;
+import liquibase.sqlgenerator.core.InsertGenerator;
+import liquibase.statement.core.InsertStatement;
+
+public class InsertGeneratorOracle extends InsertGenerator {
+    private static final int MAX_TEXT_LENGTH = 4000;
+    private static final int PRIORITY = 6;
+
+    @Override
+    public int getPriority() {
+        return PRIORITY;
+    }
+
+    @Override
+    public boolean supports(InsertStatement statement, Database database) {
+        return database instanceof OracleDatabase;
+    }
+
+    protected boolean isGiantText(Object object) {
+        if (!(object instanceof String)) {
+            return false;
+        }
+        String text = (String) object;
+        return text.length() > MAX_TEXT_LENGTH;
+    }
+
+    protected boolean containsGiantText(Map<String, Object> columnValues) {
+        for (String col : columnValues.keySet()) {
+            Object value = columnValues.get(col);
+            if (isGiantText(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected String getInsertInto(Database database, InsertStatement statement) {
+        String escapedTableName = database.escapeTableName(statement.getSchemaName(), statement.getTableName());
+        StringBuilder sb = new StringBuilder();
+        sb.append("INSERT INTO ");
+        sb.append(escapedTableName);
+        sb.append(" (");
+        return sb.toString();
+    }
+
+    protected String getColumns(Database database, InsertStatement statement) {
+        String schemaName = statement.getSchemaName();
+        String tableName = statement.getTableName();
+        Set<String> columnNames = statement.getColumnValues().keySet();
+        StringBuilder sb = new StringBuilder();
+        for (String columnName : columnNames) {
+            String escapedColumnName = database.escapeColumnName(schemaName, tableName, columnName);
+            sb.append(escapedColumnName + ", ");
+        }
+        sb.deleteCharAt(sb.lastIndexOf(" "));
+        sb.deleteCharAt(sb.lastIndexOf(","));
+        return sb.toString();
+    }
+
+    @Override
+    public Sql[] generateSql(InsertStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain) {
+        // Determine if this insert statement requires special handling
+        boolean containsGiantText = containsGiantText(statement.getColumnValues());
+
+        // If not, no special handling required
+        if (!containsGiantText) {
+            return super.generateSql(statement, database, sqlGeneratorChain);
+        }
+
+        // Create custom SQL for values bigger than Oracle's 4K limit for SQL statements
+        StringBuffer sb = new StringBuffer();
+        sb.append(getInsertInto(database, statement));
+        sb.append(getColumns(database, statement));
+        sb.append(") VALUES (");
+
+        for (String column : statement.getColumnValues().keySet()) {
+            // Pull out the raw value
+            Object rawValue = statement.getColumnValues().get(column);
+            // Convert it to an SQL friendly value
+            String sqlValue = getSqlValue(rawValue, database);
+            // Add it to the SQL
+            sb.append(sqlValue);
+            // Append the delimiter
+            sb.append(", ");
+        }
+
+        // Remove the trailing whitespace and comma
+        sb.deleteCharAt(sb.lastIndexOf(" "));
+        sb.deleteCharAt(sb.lastIndexOf(","));
+
+        // Close off the sql
+        sb.append(")");
+
+        List<NameValue> giantTextFields = getGiantTextFields(statement.getColumnValues());
+        List<Sql> list = new ArrayList<Sql>();
+        Sql sql = new UnparsedSql(sb.toString());
+        list.add(sql);
+        list.addAll(getSql(giantTextFields, database, statement));
+        return list.toArray(new Sql[list.size()]);
+    }
+
+    protected List<Sql> getSql(List<NameValue> giantTextFields, Database database, InsertStatement statement) {
+        List<Sql> sql = new ArrayList<Sql>();
+        for (NameValue giantTextField : giantTextFields) {
+            sql.addAll(getSql(giantTextField, database, statement));
+        }
+        return sql;
+
+    }
+
+    protected List<Sql> getSql(NameValue giantField, Database database, InsertStatement statement) {
+        List<Sql> sql = new ArrayList<Sql>();
+        int chunkCount = getChunkCount(giantField.getValue());
+        int length = giantField.getValue().length();
+        StringBuilder sb = new StringBuilder();
+        sb.append("-- Length: " + length + "\n");
+        sb.append("-- Chunks: " + chunkCount + "\n");
+        for (int i = 0; i < chunkCount; i++) {
+            sb.append(getOraclePlSqlFragment(i, giantField, database, statement));
+            sql.add(new UnparsedSql(sb.toString()));
+            sb = new StringBuilder();
+        }
+        return sql;
+    }
+
+    protected List<NameValue> getGiantTextFields(Map<String, Object> columnValues) {
+        List<NameValue> list = new ArrayList<NameValue>();
+        for (Map.Entry<String, Object> entry : columnValues.entrySet()) {
+            // Pull out the raw value
+            Object rawValue = entry.getValue();
+            if (!isGiantText(rawValue)) {
+                continue;
+            }
+            String text = (String) rawValue;
+            list.add(new NameValue(entry.getKey(), text));
+        }
+        return list;
+    }
+
+    protected String getData(String s, int chunkIndex) {
+        int length = s.length();
+        int beginIndex = chunkIndex * MAX_TEXT_LENGTH;
+        int endIndex = Math.min(beginIndex + MAX_TEXT_LENGTH, length);
+        return s.substring(beginIndex, endIndex);
+    }
+
+    protected String getOraclePlSqlFragment(int chunkIndex, NameValue pair, Database database, InsertStatement statement) {
+        String schemaName = statement.getSchemaName();
+        String tableName = statement.getTableName();
+        String escapedTableName = database.escapeTableName(schemaName, tableName);
+        String data = getData(pair.getValue(), chunkIndex);
+        StringBuilder sb = new StringBuilder();
+        sb.append("DECLARE    data CLOB; buffer VARCHAR2(32000);\n");
+        sb.append("BEGIN\n");
+        sb.append("    SELECT " + pair.getName() + " INTO data FROM " + escapedTableName + "\n");
+        sb.append("    WHERE \n");
+        sb.append(" MSG_QUE_ID = 2340    FOR UPDATE;\n");
+        sb.append("    buffer := '" + data + "';\n");
+        sb.append("    DBMS_LOB.writeappend(data,LENGTH(buffer),buffer);\n");
+        sb.append("END;");
+        return sb.toString();
+    }
+
+    protected int getChunkCount(String value) {
+        int length = value.length();
+        double count = Math.ceil((length * 1D) / MAX_TEXT_LENGTH);
+        return new Double(count).intValue();
+    }
+
+    protected String getSqlValue(Object newValue, Database database) {
+        TypeConverter converter = TypeConverterFactory.getInstance().findTypeConverter(database);
+        if (newValue == null || newValue.toString().equalsIgnoreCase("NULL")) {
+            return "NULL";
+        } else if (isGiantText(newValue)) {
+            return "EMPTY_CLOB()";
+        } else if (newValue instanceof String && database.shouldQuoteValue(((String) newValue))) {
+            return "'" + database.escapeStringForDatabase((String) newValue) + "'";
+        } else if (newValue instanceof Date) {
+            return database.getDateLiteral(((Date) newValue));
+        } else if (newValue instanceof Boolean) {
+            if (((Boolean) newValue)) {
+                return converter.getBooleanType().getTrueBooleanValue();
+            } else {
+                return converter.getBooleanType().getFalseBooleanValue();
+            }
+        } else {
+            return newValue.toString();
+        }
+    }
+}
