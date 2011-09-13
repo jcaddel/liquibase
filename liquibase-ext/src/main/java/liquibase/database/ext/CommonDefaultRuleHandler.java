41924996946aa9ae1a9138cf070f62eb706a4ba9
Jeff Caddel <jcaddel@yahoo.com>
2011-09-12 17:46:24 -0700

    Javadoc comment

--- liquibase-ext/src/main/java/liquibase/database/ext/CommonDefaultRuleHandler.java
+++ liquibase-ext/src/main/java/liquibase/database/ext/CommonDefaultRuleHandler.java
@@ -2,6 +2,14 @@ package liquibase.database.ext;
 
 import liquibase.database.structure.ForeignKeyConstraintType;
 
+/**
+ * When you create a foreign key in either MySQL or Oracle the same default behavior is applied by both database
+ * engines. The ON DELETE and ON UPDATE rules are assumed to be "RESTRICT" unless you specify otherwise. "RESTRICT" is
+ * also a synonym for "NO ACTION". Both of these rules mean that you cannot delete a parent record that has child
+ * records. This is a helper class for converting this information to XML in a standard way. This extension to Liquibase
+ * uses the keyword "RESTRICT" if the underlying database thinks "RESTRICT" and "NO ACTION" are synonyms and is also the
+ * default behavior for that RDBMS.
+ */
 public class CommonDefaultRuleHandler {
 
     public ForeignKeyConstraintType getDefaultUpdateRule() {
